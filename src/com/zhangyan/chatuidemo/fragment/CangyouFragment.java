c1cea120e38de53b5a4a6abca0b2ff615135398b
tingxingge1234 <88075728@qq.com>
2016-06-02 19:16:57 +0800

    一、布局 1、撰写mainActivity 2、撰写fragment_message 3、撰写fragment_cangyou 4、撰写activity_add_contact 5、撰写activity_add_public_group 6、撰写row_add_group 7、撰写fragment_groups 8、撰写menu中context_contact_list 9、撰写activity_new _group 10、撰写activity_new_group_one

---
+++ src/com/zhangyan/chatuidemo/fragment/CangyouFragment.java
@@ -0,0 +1,31 @@
+package com.zhangyan.chatuidemo.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.easemob.chatuidemo.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CangyouFragment extends Fragment {
+
+
+    public CangyouFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cangyou, container, false);
+    }
+
+}
